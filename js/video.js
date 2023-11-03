54b476a45e0bc795d95841a08dc9a21719294ac9
ManojRdk <140079622+ManojRdk@users.noreply.github.com>
2023-11-03 22:59:02 +0530

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,251 @@
+
+//designn
+//Photoshop
+const Alightmotion= document.getElementById('Alightmotion')
+Alightmotion.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+      url: 'https://alight-motion.com/',
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+//Adobeillustrator
+const Inshot= document.getElementById('Inshot')
+Inshot.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+
+      url: 'https://www.inshot.com/'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+//Adobeindesign
+const VNapp= document.getElementById('VNapp')
+VNapp.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+ 
+      url: 'https://www.vlognow.me/'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+//Canva
+const noizz= document.getElementById('noizz')
+noizz.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+
+      url: 'https://play.google.com/store/apps/details?id=com.yy.biu'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+//Xara
+const Vita= document.getElementById('Vita')
+Vita.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+    
+      url: 'https://play.google.com/store/apps/details?id=com.snowcorp.vita&pcampaignid=web_share'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+//Vectr
+const Aftereffects= document.getElementById('Aftereffects')
+Aftereffects.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+
+      url: 'https://www.adobe.com/in/products/aftereffects/campaign/pricing.html?sdid=L3XTTPNV&mv=search&mv2=paidsearch&ef_id=8d2b91afe61e1ac318f0f4edaa149516:G:s&s_kwcid=AL!3085!10!78752656709936!78752878555060&msclkid=8d2b91afe61e1ac318f0f4edaa149516&utm_source=bing&utm_medium=cpc&utm_campaign=IN%7CCPRO%7CVideo%7CCORE%7CAE_hv_Exact%7CBNG%7C%7C&utm_term=after%20effects&utm_content=in%3Cae%3Ccons%3Cbr%3C_hv%3C%3C%3Cexact'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+//Sketch
+const kinemasster= document.getElementById('kinemasster')
+kinemasster.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+ 
+      url: 'https://www.kinemaster.com/en'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+//Figma
+const Video= document.getElementById('Video')
+Video.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+  
+      url: 'https://www.movavi.com/video-editor-plus/in/?utm_campaignid=482887936&utm_adgroupid=1225956456787361&msclkid=01cb6dae83901a1dab2f08eccf6328ca&utm_source=bing&utm_medium=cpc&utm_campaign=Win%20Video%20Plus%20Editor%20%2B%20SSM%20%28%2BMac,%20Free%29%20-%20India%20-%20EN%20-%20Filbert%20-%20SEARCH&utm_term=video%20editor%20app&utm_content=VIDEO%20EDITOR'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+//Inkscape
+const Filmora= document.getElementById('Filmora')
+Filmora.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+   
+      url: 'https://filmora.wondershare.net/filmora-video-editor.html?msclkid=46bd16f24bad1197d72a73b006d5ecab&utm_source=bing&utm_medium=cpc&utm_campaign=Filmora_Brand_SS_All3_IN_Pid(1901)_Bing-PC&utm_term=filmora&utm_content=1.%20Brand-Filmora%20skag'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+//Wix
+const Makevideo= document.getElementById('Makevideo')
+Makevideo.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+      
+      url: 'https://clideo.com/video-maker'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+//Adobeexpress
+const vivacut= document.getElementById('vivacut')
+vivacut.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+ 
+      url: 'http://www.vivacut.com/'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+//Snappa
+const VSCO= document.getElementById('VSCO')
+VSCO .addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+      url: 'https://vsco.co/download'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+//Snappa
+const videoleap= document.getElementById('videoleap')
+videoleap .addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+      url: 'https://www.videoleapapp.com/'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+//Pixler
+const Magisto= document.getElementById('Magisto')
+Magisto.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+     
+      url: 'https://www.magisto.com/didit'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+const Beatsync= document.getElementById('Beatsync')
+Beatsync.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+     
+      url: 'https://play.google.com/store/apps/details?id=com.kinemaster.app.beatsync&pcampaignid=web_share'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+const Beatly= document.getElementById('Beatly')
+Beatly.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+     
+      url: 'https://play.google.com/store/apps/details?id=vinkle.video.editor&pcampaignid=web_share'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+const Youcut= document.getElementById('Youcut')
+Youcut.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+     
+      url: 'https://youcut.pro/'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
